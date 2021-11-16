3957571d7a9dce0f715722c13747cc1c124d6cea
keg7723 <dmsrbrjek1@naver.com>
2021-11-16 16:19:41 +0900

    all:

---
+++ JavaScript/index.js
@@ -0,0 +1,21 @@
+   
+    //  window.onload = function init(){
+        window.addEventListener("load",function(){
+
+            var btnPrint = document.getElementById("btn-print");
+            var add = function(x,y){
+                return x + y;
+            };
+            btnPrint.onclick = function()  {
+           //  var btnPrint = document.getElementById("btn-print"); 
+            var x = prompt("x값을 입력하세요",0);
+            var y = prompt("y값을 입력하세요",0);
+             x = parseInt(x);
+             y = parseInt(y);
+             
+             btnPrint.value = x+y;
+            //  alert(x+y);
+        };
+    });   
+
+    
